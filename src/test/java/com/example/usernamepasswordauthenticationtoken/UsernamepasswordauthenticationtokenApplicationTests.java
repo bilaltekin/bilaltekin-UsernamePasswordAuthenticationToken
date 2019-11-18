45df32b00f7af164a45819a01c21083b0fcb3890
Bilal Tekin <bt.ce21@gmail.com>
2019-11-18 16:25:42 +0300

    first commit

---
+++ src/test/java/com/example/usernamepasswordauthenticationtoken/UsernamepasswordauthenticationtokenApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.usernamepasswordauthenticationtoken;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsernamepasswordauthenticationtokenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
